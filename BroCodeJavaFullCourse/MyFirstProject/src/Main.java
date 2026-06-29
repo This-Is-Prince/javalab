606c3a5496fe0f0471133331f96c11ef3496b470
This-Is-Prince <this.is.kumar.prince@gmail.com>
2026-06-29 21:44:21 +0530

    feat: MyFirstProject completed

---
+++ BroCodeJavaFullCourse/MyFirstProject/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+//        THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+        This is multiline comment
+        */
+
+        System.out.print("I like Pizza!\n");
+        System.out.print("It's really good!\n");
+        System.out.print("Buy me pizza!");
+    }
+}
